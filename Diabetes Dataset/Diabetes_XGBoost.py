7b3123564ed504346eceda29999e930200044268
Mridul Sanghavi <mridulsanghavi.17@gmail.com>
2024-03-22 11:13:42 +0530

    Increasing accuracy

--- Diabetes Dataset/Diabetes_XGBoost.py
+++ Diabetes Dataset/Diabetes_XGBoost.py
@@ -7,7 +7,6 @@ diabetes_data = pd.read_csv(r'C:\Users\mridu\Documents\GitHub Repos\XG-Boost\Dia
 
 # Display the first few rows of the dataset
 print(diabetes_data.head())
-
 # Assuming the target variable is 'Outcome' (as we are performing regression)
 X = diabetes_data.drop('Outcome', axis=1)  # Features
 y = diabetes_data['Outcome']                # Target variable
@@ -16,7 +15,9 @@ y = diabetes_data['Outcome']                # Target variable
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Instantiate XGBoost classifier
-classifier = xgb.XGBClassifier(objective='binary:logistic', random_state=42)
+number_Boosts=5000
+classifier = xgb.XGBClassifier(objective='binary:logistic', random_state=42, n_estimators=number_Boosts)
+#trying to get the number of ephochs right
 
 # Train the model
 classifier.fit(X_train, y_train)
@@ -32,3 +33,6 @@ print("Accuracy:", accuracy)
 # Save predictions to a CSV file (optional)
 predictions = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
 predictions.to_csv('diabetes_predictions.csv', index=False)
+print(number_Boosts)
+predicted_data = pd.read_csv(r'C:\Users\mridu\Documents\GitHub Repos\XG-Boost\Diabetes Dataset\diabetes_predictions.csv')
+print(predicted_data)
